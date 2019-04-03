a17131481a44a8ead88d4753eacea28f55a86c1a
Jessica Hong <jessicahong.24@gmail.com>
2019-04-03 19:34:04 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,7 +2,7 @@
 # See README.md for instructions on how to do this
 
 require_relative './spec_helper.rb'
-require_relative './specfizzbuzz_spec.rb'
+require_relative './fizzbuzz_spec.rb'
 
 def fizzbuzz (num = )
   if num % 3 == 0 
